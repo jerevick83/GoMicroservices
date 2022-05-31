d81eb0e4ff09856e349753f3949f1ebcc5b46cb6
jerevick83 <57697065+jerevick83@users.noreply.github.com>
2022-05-31 04:51:00 +0000

    Go Validator

--- data/products.go
+++ data/products.go
@@ -2,16 +2,18 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/go-playground/validator/v10"
 	"io"
 	"time"
 )
 
 type Product struct {
 	ID          int     `json:"id"`
-	Name        string  `json:"name"`
+	Name        string  `json:"name" validate:"required,gte=10"`
 	Description string  `json:"description"`
 	SKU         string  `json:"-"`
-	Price       float64 `json:"price"`
+	Price       float64 `json:"price" validate:"gt=0"`
 	CreatedOn   string  `json:"_"`
 	DeleteOn    string  `json:"_"`
 	UpdatedOn   string  `json:"_"`
@@ -31,6 +33,27 @@ func (p *Product) FromJSON(w io.Reader) error {
 func AddProduct(p *Product) {
 	p.ID = getNextID()
 	productList = append(productList, p)
+}
+
+func UpdateProduct(id int, p *Product) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+	p.ID = id
+	productList[pos] = p
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrProductNotFound
 }
 
 func getNextID() int {
@@ -58,4 +81,9 @@ var productList = []*Product{
 		CreatedOn:   time.Now().UTC().String(),
 		UpdatedOn:   time.Now().UTC().String(),
 	},
+}
+
+func (p *Product) Validate() error {
+	validate := validator.New()
+	return validate.Struct(p)
 }
--- handlers/product.go
+++ handlers/product.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/jerevick8/data"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type Products struct {
@@ -13,16 +16,8 @@ type Products struct {
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l: l}
 }
-func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		p.getProducts(w, r)
-	}
-	if r.Method == "POST" {
-		p.addProducts(w, r)
-	}
-}
 
-func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
+func (p *Products) GetProducts(w http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handle GET Products")
 	lp := data.GetProducts()
 	err := lp.ToJSON(w)
@@ -31,13 +26,49 @@ func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (p *Products) addProducts(w http.ResponseWriter, r *http.Request) {
+func (p *Products) AddProducts(w http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handle Post Product")
 	newProduct := &data.Product{}
 	err := newProduct.FromJSON(r.Body)
 	if err != nil {
 		http.Error(w, "unable to unmarshal product", http.StatusBadRequest)
 	}
+	err = newProduct.Validate()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Validation Error: %s", err), http.StatusBadRequest)
+		return
+	}
 	data.AddProduct(newProduct)
-	p.getProducts(w, r)
+	p.GetProducts(w, r)
+}
+
+func (p *Products) UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Request")
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Unable to convert id from string to int", http.StatusBadRequest)
+	}
+	p.l.Println("ID: ", id)
+	prod := &data.Product{}
+
+	err = prod.FromJSON(r.Body)
+	err = prod.Validate()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Validation Error: %s", err), http.StatusBadRequest)
+		return
+	}
+	if err != nil {
+		http.Error(w, "unable to marshal product", http.StatusBadRequest)
+	}
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(w, "Product not found", http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, "Product not found", http.StatusInternalServerError)
+		return
+	}
+	p.GetProducts(w, r)
 }
